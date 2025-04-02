93a3b48b8158e9962a7825d66b229a3ef4e7491b
13v13reddy <bobba.brahmareddy@gmail.com>
2025-04-02 12:30:01 -0400

    Add Spring Boot support with main application class and success endpoint

--- src/main/java/pkg/X.java
+++ src/main/java/pkg/X.java
@@ -1,10 +1,27 @@
 package pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * A utility.
  */
+@SpringBootApplication
 public class X {
+    public static void main(String[] args) {
+        SpringApplication.run(X.class, args);
+    }
+}
+
+@RestController
+class SuccessController {
     /**
-     * Does something.
+     * Endpoint to indicate success.
      */
-    public void someMethod() {}
+    @GetMapping("/")
+    public String success() {
+        return "Project Deployment Success!";
+    }
 }
